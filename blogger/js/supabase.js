ba4d21efc055029d009a0e6995095983db0582aa
coder8762-debug <coder8762@gmail.com>
2026-07-29 20:43:09 +0500

    Update supabase.js

--- blogger/js/supabase.js
+++ blogger/js/supabase.js
@@ -1,5 +1,3 @@
-// Supabase Configuration
-
 const SUPABASE_URL = "https://xjtfejxqghqhharucvaj.supabase.co";
 
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqdGZlanhxZ2hxaGhhcnVjdmFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMzMzNDAsImV4cCI6MjEwMDkwOTM0MH0.7biA5N-I13qeEt_2Wf7DKd1x-zoSk-wEjIg5v0wDEEA";
@@ -8,4 +6,7 @@ const supabase = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
+
+window.supabaseClient = supabase;
+
 console.log("supabase.js loaded");
