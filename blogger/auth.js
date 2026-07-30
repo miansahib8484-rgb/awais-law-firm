fdc0eae0783b88eb7652f7f9a9be6d7eda3ecf4a
coder8762-debug <coder8762@gmail.com>
2026-07-30 17:58:30 +0500

    Update auth.js

--- blogger/auth.js
+++ blogger/auth.js
@@ -1,7 +1,41 @@
-console.log("Auth JS Loaded");
-
 document.addEventListener("DOMContentLoaded", () => {
 
-    alert("JavaScript Working");
+    const form = document.getElementById("loginForm");
+
+    form.addEventListener("submit", loginUser);
 
 });
+
+async function loginUser(e) {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const message = document.getElementById("message");
+
+    message.innerHTML = "";
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password
+    });
+
+    if (error) {
+
+        message.style.color = "red";
+        message.innerHTML = error.message;
+        return;
+
+    }
+
+    message.style.color = "green";
+    message.innerHTML = "Login Successful";
+
+    setTimeout(() => {
+
+        window.location.href = "dashboard.html";
+
+    }, 1000);
+
+}
