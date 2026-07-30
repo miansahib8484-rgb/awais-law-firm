07c75cae8128b03ab043741f87d143c6070a7fe5
coder8762-debug <coder8762@gmail.com>
2026-07-30 19:34:44 +0500

    Update auth.js

--- blogger/auth.js
+++ blogger/auth.js
@@ -1,17 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-    console.log("DOM Loaded");
-
-    const form = document.getElementById("loginForm");
-
-    console.log(form);
-
-    form.addEventListener("submit", function(e){
-
-        e.preventDefault();
-
-        alert("Login Button Working");
-
-    });
-
-});
+alert("AUTH FILE IS RUNNING");
