b429684726db84a64b6ea08c18d7cf3e6b0e9151
coder8762-debug <coder8762@gmail.com>
2026-08-01 09:36:17 +0500

    Update supabase.js

--- blog2/supabase.js
+++ blog2/supabase.js
@@ -1,1 +1,10 @@
+const SUPABASE_URL = "https://vuzwaowbtmezrxspsoud.supabase.co";
 
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1endhb3didG1lenJ4c3Bzb3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0MTEzMjksImV4cCI6MjEwMDk4NzMyOX0.rZ4CVoL4wEvfPPkR2HLdMhi8z4hwrDxGynFRRpTdf4s";
+
+window.db = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
+
+console.log("Supabase Connected");
